23e6f914a9d37d34fe0f42adcc534d7c4c5160da
prado <antonio.ti.celso@gmail.com>
2021-11-29 22:50:34 -0300

    Criaçao de desafios java: ConsumoAutomovel, ContagemCedula, ExibirPar.

---
+++ dio/ExibirPar.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+// (Exibindo numeros pares)
+public class ExibirPar {
+    public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st = new StringTokenizer(br.readLine());
+
+      int linha = Integer.parseInt(st.nextToken());
+
+    	for (int i = 1; i <= linha; i++) {
+    	  if (i % 2 == 0) {
+          System.out.println(i);
+    	  }
+    	}
+    }
+    
+}
