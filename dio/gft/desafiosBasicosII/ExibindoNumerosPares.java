e0d9eb1ae0d5725a180de2831112ac9fd59c0c21
prado <antonio.ti.celso@gmail.com>
2022-02-04 16:09:55 +0000

    Criaçao Package: gft.desafiosBasicosII Criaçao class: ExibindoNumerosPares, NotacaoCientifica, NotasEMoedas

--- dio/gft/desafiosBasicosII/ExibindoNumerosPares.java
+++ dio/gft/desafiosBasicosII/ExibindoNumerosPares.java
@@ -1,18 +1,23 @@
-package dio.gft.desafiosBasicos;
+package dio.gft.desafiosBasicosII;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class VisitaNaFeira {
+public class ExibindoNumerosPares{
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int total = a + b; // Altere o valor da variável com o cálculo esperado
 
-        System.out.println("X = " + total);
+        int linha = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= linha; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
     }
+
 }
---
+++ dio/gft/desafiosBasicosII/NotacaoCientifica.java
@@ -0,0 +1,19 @@
+package dio.gft.desafiosBasicosII;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class NotacaoCientifica {
+
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+
+        float numero = leitor.nextFloat();
+        float expoente = 0;
+
+        System.out.printf("%.4f", leitor);
+        System.out.printf("E+%.2f", expoente);
+
+    }
+
+}
